dabe6600c712cc0501fc0e47151cdf68d36399ed
oravagamer <ondrejknotek87@gmail.com>
2023-11-03 20:07:10 +0100

    new(Controller_arduino): gamepad for game simple usage

--- Controller_arduino/Controller_arduino.ino
+++ Controller_arduino/Controller_arduino.ino
@@ -1,5 +1,28 @@
+#include <Arduino.h>
+#include <BleGamepad.h>
+
+#define GPIO13 13
+
+int currentState;
+
+BleGamepad bleGamepad("Orava's Wireless Gamepad", "Oravagamer", 100);
+
 void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+  bleGamepad.begin();
+  pinMode(GPIO13, INPUT_PULLUP);
 }
 
 void loop() {
+  if (bleGamepad.isConnected()) {
+    currentState = digitalRead(GPIO13);
+    
+    if (currentState == HIGH) {
+      bleGamepad.setLeftThumb(0, 32767);
+    } else if (currentState == LOW) {
+      bleGamepad.setLeftThumb(0, 0);
+    }
+
+  }
 }
