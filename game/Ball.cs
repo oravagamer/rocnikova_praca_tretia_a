3b6461c271491c26d311375d2d22038f840cab18
oravagamer <ondrejknotek87@gmail.com>
2023-11-03 20:26:52 +0100

    new(game): Ball scene created

---
+++ game/Ball.cs
@@ -0,0 +1,13 @@
+using System.Text.Json.Serialization.Metadata;
+using Godot;
+
+public partial class Ball : RigidBody2D {
+    public float Speed { get; set; } = 400;
+    public override void _Ready() {
+        
+    }
+
+    public override void _Process(double delta) {
+        
+    }
+}
