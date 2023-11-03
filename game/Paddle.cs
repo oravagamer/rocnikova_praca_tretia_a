11018634f95c702a1d3ca54285dc0ed1122706f0
oravagamer <ondrejknotek87@gmail.com>
2023-11-03 15:09:03 +0100

    new(game): basic structure of Paddle class

--- game/Paddle.cs
+++ game/Paddle.cs
@@ -1,4 +1,14 @@
 using Godot;
 
 public partial class Paddle : Area2D {
+    [Export] public int Speed { get; set; } = 400;
+    public Vector2 ScreenSize;
+
+    public override void _Ready() {
+        ScreenSize = GetViewportRect().Size;
+    }
+
+    public override void _Process(double delta) {
+        var velocity = Vector2.Zero;
+    }
 }
