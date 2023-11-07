203318b487fe031e0ce8bb23efc1cb6b84418230
oravagamer <ondrejknotek87@gmail.com>
2023-11-07 19:25:35 +0100

    new(Controller_arduino): new folder for controller design and changes in code

--- Controller_arduino/Controller_arduino.ino
+++ Controller_arduino/Controller_arduino.ino
@@ -1,28 +1,30 @@
 #include <Arduino.h>
 #include <BleGamepad.h>
 
-#define GPIO13 13
+#define STICK_PIN 13
+#define BLE_LED_PIN 12
+#define BLINK_INTERVAL 1000
 
-int currentState;
-
+int passedMillis = 0;
 BleGamepad bleGamepad("Orava's Wireless Gamepad", "Oravagamer", 100);
 
 void setup() {
   Serial.begin(115200);
+  pinMode(BLE_LED_PIN, OUTPUT);
+  digitalWrite(BLE_LED_PIN, LOW);
   Serial.println("Starting BLE work!");
   bleGamepad.begin();
-  pinMode(GPIO13, INPUT_PULLUP);
 }
 
 void loop() {
   if (bleGamepad.isConnected()) {
-    currentState = digitalRead(GPIO13);
-    
-    if (currentState == HIGH) {
-      bleGamepad.setLeftThumb(0, 32767);
-    } else if (currentState == LOW) {
-      bleGamepad.setLeftThumb(0, 0);
+    digitalWrite(BLE_LED_PIN, HIGH);
+//    bleGamepad.setLeftThumb(0, 32767);
+  } else {
+    auto currentMillis = millis();
+    if (currentMillis - passedMillis >= BLINK_INTERVAL) {
+      digitalWrite(BLE_LED_PIN, !digitalRead(BLE_LED_PIN));
+      passedMillis = currentMillis;
     }
-
   }
 }
